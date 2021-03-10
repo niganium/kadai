e4e515674fd5390ec53f0e8457c53bf158e4e59a
nishi <sonnabakananz@gmail.com>
2021-03-10 15:45:27 +0900

    add

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Bookers2Ver2
     config.paths.add 'lib', eager_load: true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.active_support.use_authenticated_message_encryption = faslse
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
