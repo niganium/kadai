b5911cc5f093d8ada3d259edc35c4eeb9f546c45
vagrant <vagrant@localhost.localdomain>
2021-03-11 08:21:21 +0000

    test

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,7 @@ set :runner_command, "rails runner"
 set :environment, rails_env
 set :output, 'log/cron.log'
 
-every 1.minute, at: '9:00 am' do
+every 1.day, at: '9:00 am' do
   begin
     runner "DailyMailer.all_notify"
   rescue => e
